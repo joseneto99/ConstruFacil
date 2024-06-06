5ade67462a45f826b335bf586695ba0f470a5611
joseneto99 <jose.2019180360@unicap.br>
2024-06-06 11:09:02 -0300

    xx

---
+++ cypress/e2e/3-testando-vercel.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress"/>
+
+describe('Testando Acesso no Verecel', () => {
+    it('Primeiro Uso do Cypress', () =>{
+        cy.visit("https://contrufacilas.vercel.app/")
+    });
+});
